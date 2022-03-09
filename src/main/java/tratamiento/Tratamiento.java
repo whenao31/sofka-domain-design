1f7c63c746d3a0aef1eba689d6e0f1678d4cb087
wil <whenao86@gmail.com>
2022-03-08 20:20:51 -0500

    Crear AR-Tratamiento y entidad Cotizacion.
    -Crear Value Objects: Fecha, Hora, Dinero, CitaId, Factura y TratamientoId.
    -Crear comportamientos de Cita: confirmarCita(), cancelarCita() y modificarFechaYHora()

--- src/main/java/tratamiento/Tratamiento.java
+++ src/main/java/tratamiento/Tratamiento.java
@@ -1,4 +1,15 @@
 package tratamiento;
 
-public class Tratamiento {
+import co.com.sofka.domain.generic.AggregateEvent;
+import tratamiento.valueobject.TratamientoId;
+
+import java.util.Map;
+
+public class Tratamiento extends AggregateEvent<TratamientoId> {
+
+    protected Map<String, Cita> citas;
+
+    public Tratamiento(TratamientoId tratamientoId) {
+        super(tratamientoId);
+    }
 }
